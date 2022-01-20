c44a0928acd75bd9cf0a7638f4ffb23db49db96b
Edward Robinson <eddiesr1234@gmail.com>
2022-01-20 17:57:18 -0500

    removed logging to file logic

--- server/src/app.js
+++ server/src/app.js
@@ -1,20 +1,14 @@
 const express = require("express");
-// const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
-// const morgan = require("morgan");
+const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
 
 app.use(cors({ origin: "http://localhost:3000" }));
-// const accessLogStream = fs.createWriteStream(
-//   path.join(__dirname, "access.log"),
-//   { flags: "a" }
-// );
-
-// app.use(morgan("combined", { stream: accessLogStream }));
+app.use(morgan("combined"));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
