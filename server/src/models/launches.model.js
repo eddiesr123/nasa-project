36a00bd40e84fefc720ebb3038299961d7e39d62
Edward Robinson <eddiesr1234@gmail.com>
2022-01-21 18:12:08 -0500

    added post launch end point, updated client post request

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,16 +1,39 @@
 const launches = new Map();
 
-const launch = {
+let flightNbr = 100;
+
+const firstLaunch = {
   flightNumber: 100,
   mission: "Kepler Exploration X",
   rocket: "Explorer IS1",
   launchDate: new Date("December 27, 2030"),
-  destination: "Kepler-442 b",
-  customer: ["ZTM", "Nasa"],
+  target: "Kepler-442 b",
+  customer: ["ZTM", "NASA"],
   upcoming: true,
   success: true,
 };
 
-launches.set(launch.flightNumber, launch);
+launches.set(firstLaunch.flightNumber, firstLaunch);
 
-module.exports = { launches };
+function getAllLaunches() {
+  return Array.from(launches.values());
+}
+
+function addNewLaunch(launch) {
+  flightNbr++;
+
+  const newLaunch = {
+    flightNumber: flightNbr,
+    ...launch,
+    launchDate: new Date(launch.launchDate),
+    customer: ["ZTM", "NASA"],
+    upcoming: true,
+    success: true,
+  };
+
+  launches.set(flightNbr, newLaunch);
+
+  return newLaunch;
+}
+
+module.exports = { getAllLaunches, addNewLaunch };
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,7 +1,29 @@
-const { launches } = require("../../models/launches.model");
+const { getAllLaunches, addNewLaunch } = require("../../models/launches.model");
 
-function getAllLaunches(req, res) {
-  return res.status(200).json(Array.from(launches.values()));
+function httpGetAllLaunches(req, res) {
+  return res.status(200).json(getAllLaunches());
 }
 
-module.exports = { getAllLaunches };
+function httpAddNewLaunch(req, res) {
+  const launch = req.body;
+
+  const requestNotValid =
+    !launch.mission || !launch.rocket || !launch.launchDate || !launch.target;
+
+  if (requestNotValid) {
+    return res
+      .status(400)
+      .json({ error: "Missing required launch properties" });
+  }
+
+  const dateNotValid = isNaN(new Date(launch.launchDate));
+
+  if (dateNotValid) {
+    return res.status(400).json({ error: "Invalid launch date" });
+  }
+
+  const newLaunch = addNewLaunch(launch);
+  return res.status(201).json(newLaunch);
+}
+
+module.exports = { httpGetAllLaunches, httpAddNewLaunch };
