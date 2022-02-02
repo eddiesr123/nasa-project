67a63f3a65b00c12a1420adf307c1ed121e9ebc3
Edward Robinson <eddiesr1234@gmail.com>
2022-02-02 11:35:48 -0500

    test ggshield/pre commit

--- server/src/server.js
+++ server/src/server.js
@@ -9,6 +9,8 @@ const { loadPlanetsData } = require("./models/planets.model");
 const PORT = process.env.PORT || 8000;
 
 const MONGO_URL = process.env.MONGO_URL;
+const testPrecommit =
+  "mongodb+srv://nasa-api:veJOhTD2itSqUhwd@nasacluster.n74nt.mongodb.net/nasa?retryWrites=true&w=majority";
 
 const server = http.createServer(app);
 
