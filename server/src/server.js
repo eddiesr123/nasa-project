ca25755af1c98365fe9ba5a5a71194018189e4bc
Edward Robinson <eddiesr1234@gmail.com>
2022-02-02 12:20:53 -0500

    ggshield/pre-commit integration successful

--- server/src/server.js
+++ server/src/server.js
@@ -9,7 +9,6 @@ const { loadPlanetsData } = require("./models/planets.model");
 const PORT = process.env.PORT || 8000;
 
 const MONGO_URL = process.env.MONGO_URL;
-const AWS_KEY = "lkgjg34589ut3480tu34jijtj3490-tyu349-ti349-9j034t6-igk";
 
 const server = http.createServer(app);
 
